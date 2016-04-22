905aaa51a70bebfb693425d2a2762d98105073e5
Debbie Wood <wood.debbie@gmail.com>
2016-04-22 08:10:37 +0100

    redis - wip

--- level7-persistance-redis/app.js
+++ level7-persistance-redis/app.js
@@ -6,12 +6,21 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io')(server);
 
+var redis = require('redis');
+
+var redisClient = redis.createClient();
+
 var messages = [];
 var storeMessage = function(name, data){
-    messages.push({name: name, data: data});
-    if(messages.length > 10){
-        messages.shift();
-    }
+    //messages.push({name: name, data: data});
+    //if(messages.length > 10){
+    //    messages.shift();
+    //}
+
+    var message = JSON.stringify({name:name, data:data});
+    redisClient.lpush("messages",message, function(err, response){
+       redisClient.ltrim("messages",0,9); // keeps newest 10 items
+    });
     console.log("storing "+name+" "+data);
 }
 
@@ -28,17 +37,39 @@ io.on('connection', function(client){ // listen to socket connection from a clie
 
    client.on('joins', function(data){ // if the client emits a join, do this:
 
-       console.log(messages.length);
-       messages.forEach(function(message){ // send the history of recent chats
-           //console.log(message);
-           client.emit('saying',message.name + " : " +message.data);
+       redisClient.lrange("messages",0,-1, function(err, messages){
+         messages = messages.reverse();
+         console.log(messages.length);
+         messages.forEach(function(message){ // send the history of recent chats
+               message = JSON.parse(message);//parse into JSON object
+               client.emit('saying',message.name + " : " +message.data);
+           });
+
        });
+
 
       //console.log(data+" has joined");
       client.nickname = data; // this is available both to the server and the client
       client.broadcast.emit('joins', data); // broadcast to all clients that someone has joined
+       client.broadcast.emit('add chatter', data);
+       redisClient.smembers('names', function(names){
+           if(names) {
+               names.forEach(function (name) {
+                   client.emit('add chatter', name);
+               });
+           }
+       });
+       redisClient.sadd("chatters", data);
 
    });
+
+    client.on('disconnect', function(name){
+        var nickname = client.nickname;
+
+            client.broadcast.emit("remove chatter", nickname);
+            redisClient.srem("chatters", nickname);
+
+    });
 
    client.on('saying', function(data){ // if a client emits a saying event: do this
        var nickname = client.nickname;
